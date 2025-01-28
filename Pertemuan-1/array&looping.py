7c1a5742d0d048249642d8487078772c5989418d
Jery-Manuhutu <jery.ichigo.manuhutu@gmail.com>
2025-01-28 20:00:51 +0700

    Percobaan 2
    indent error

--- Pertemuan-1/array&looping.py
+++ Pertemuan-1/array&looping.py
@@ -3,6 +3,6 @@ Arr2 = []
 print(f'Isi Array Awal = {Arr1}')
 i = len(Arr1) - 1
 while i >= 0 :
-Arr2.append(Arr1[i])
-i-=1
+  Arr2.append(Arr1[i])
+  i-=1
 print(f'Isi Array setelah dibalik = {Arr2}')
