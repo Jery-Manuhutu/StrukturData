0b541b8e08eeb3496c13c6661eb8de297dc0864d
Jery-Manuhutu <jery.ichigo.manuhutu@gmail.com>
2025-01-28 19:54:47 +0700

    percobaan 2

--- Pertemuan-1/array&looping.py
+++ Pertemuan-1/array&looping.py
@@ -1,1 +1,8 @@
-
+Arr1 = [10,2,43,24,58,32,63,29,98,71]
+Arr2 = []
+print(f'Isi Array Awal = {Arr1}')
+i = len(Arr1) - 1
+while i >= 0 :
+Arr2.append(Arr1[i])
+i-=1
+print(f'Isi Array setelah dibalik = {Arr2}')
